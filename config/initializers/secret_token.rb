37b00f130d0f058d1b0a01340708256dea3f32b2
myun2 <myun2@nwhite.info>
2013-04-23 16:14:21 +0900

    initial commit (basho)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basho::Application.config.secret_token = '7ef88cd91a50acd0ecc005af7ddaa0c23ebaa20bab84054717b62ab9793e6f414d0136374023937fe0e2287e78e90501324a69275286a6ec8444577eab3dfc49'
